fe3a702c1b59d4958a2b4b21588f78c47eb1eed8
Anees123Najar <anis.najjar9@gmail.com>
2018-10-19 09:31:44 +0300

    19/10/18

--- app/src/main/java/com/example/user/ansprj/wallActvty.java
+++ app/src/main/java/com/example/user/ansprj/wallActvty.java
@@ -2,6 +2,10 @@ package com.example.user.ansprj;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class wallActvty extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class wallActvty extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wall_actvty);
+        GridView gridvw = (GridView) findViewById(R.id.gridvw);
+        gridvw.setAdapter(new ImageAdapter(this));
+
+        gridvw.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(wallActvty.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
