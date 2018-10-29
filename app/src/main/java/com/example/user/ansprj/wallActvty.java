73342aefd6f5a53664d3376e3401d10a0b46d05b
Anees123Najar <anis.najjar9@gmail.com>
2018-10-29 13:29:53 +0200

    29/10/18 lesson 13:29

--- app/src/main/java/com/example/user/ansprj/wallActvty.java
+++ app/src/main/java/com/example/user/ansprj/wallActvty.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.Toast;
@@ -11,6 +12,7 @@ import android.widget.Toast;
 public class wallActvty extends AppCompatActivity
 {
     private ImageButton imgBtnBks;
+    private Button signOutBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
