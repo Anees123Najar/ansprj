09019a4c2396a14234c584cf1074788908b58ab2
Anees123Najar <anis.najjar9@gmail.com>
2018-10-29 08:22:49 +0200

    29/10/18 lesson 08:22

--- app/build/generated/source/r/debug/android/support/coordinatorlayout/R.java
+++ app/build/generated/source/r/debug/android/support/coordinatorlayout/R.java
@@ -61,18 +61,18 @@ public final class R {
         public static final int notification_top_pad_large_text = 0x7f0600cf;
     }
     public static final class drawable {
-        public static final int notification_action_background = 0x7f070080;
-        public static final int notification_bg = 0x7f070081;
-        public static final int notification_bg_low = 0x7f070082;
-        public static final int notification_bg_low_normal = 0x7f070083;
-        public static final int notification_bg_low_pressed = 0x7f070084;
-        public static final int notification_bg_normal = 0x7f070085;
-        public static final int notification_bg_normal_pressed = 0x7f070086;
-        public static final int notification_icon_background = 0x7f070087;
-        public static final int notification_template_icon_bg = 0x7f070088;
-        public static final int notification_template_icon_low_bg = 0x7f070089;
-        public static final int notification_tile_bg = 0x7f07008a;
-        public static final int notify_panel_notification_icon_bg = 0x7f07008b;
+        public static final int notification_action_background = 0x7f070081;
+        public static final int notification_bg = 0x7f070082;
+        public static final int notification_bg_low = 0x7f070083;
+        public static final int notification_bg_low_normal = 0x7f070084;
+        public static final int notification_bg_low_pressed = 0x7f070085;
+        public static final int notification_bg_normal = 0x7f070086;
+        public static final int notification_bg_normal_pressed = 0x7f070087;
+        public static final int notification_icon_background = 0x7f070088;
+        public static final int notification_template_icon_bg = 0x7f070089;
+        public static final int notification_template_icon_low_bg = 0x7f07008a;
+        public static final int notification_tile_bg = 0x7f07008b;
+        public static final int notify_panel_notification_icon_bg = 0x7f07008c;
     }
     public static final class id {
         public static final int action_container = 0x7f08000d;
@@ -81,13 +81,13 @@ public final class R {
         public static final int action_text = 0x7f080016;
         public static final int actions = 0x7f080017;
         public static final int async = 0x7f08001f;
-        public static final int blocking = 0x7f080024;
-        public static final int bottom = 0x7f080025;
-        public static final int chronometer = 0x7f08002c;
-        public static final int end = 0x7f080043;
-        public static final int forever = 0x7f08004e;
-        public static final int icon = 0x7f080056;
-        public static final int icon_group = 0x7f080057;
+        public static final int blocking = 0x7f080023;
+        public static final int bottom = 0x7f080024;
+        public static final int chronometer = 0x7f08002b;
+        public static final int end = 0x7f080042;
+        public static final int forever = 0x7f08004d;
+        public static final int icon = 0x7f080055;
+        public static final int icon_group = 0x7f080056;
         public static final int info = 0x7f08005b;
         public static final int italic = 0x7f08005c;
         public static final int left = 0x7f080060;
--- app/src/main/java/com/example/user/ansprj/wallActvty.java
+++ app/src/main/java/com/example/user/ansprj/wallActvty.java
@@ -5,14 +5,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
-public class wallActvty extends AppCompatActivity {
-
+public class wallActvty extends AppCompatActivity
+{
+    private ImageButton imgBtnBks;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wall_actvty);
+
 
     }
 }
