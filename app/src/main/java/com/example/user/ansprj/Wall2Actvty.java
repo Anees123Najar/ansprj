c9efde69940a8546f86315b70a0c0915def1b6a9
Anis123Najar <anis.najjar9@gmail.com>
2019-01-30 12:16:23 +0200

    30/01/19

--- app/src/main/java/com/example/user/ansprj/Wall2Actvty.java
+++ app/src/main/java/com/example/user/ansprj/Wall2Actvty.java
@@ -1,14 +1,16 @@
 package com.example.user.ansprj;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class Wall2Actvty extends AppCompatActivity implements View.OnClickListener {
-    private ImageButton imgBtnBks;
+    private ImageView imgBtnBks;
     private Button signOutBtn;
     FirebaseAuth fAuth;// = FirebaseAuth.getInstance();
     @Override
@@ -18,6 +20,8 @@ public class Wall2Actvty extends AppCompatActivity implements View.OnClickListen
         signOutBtn=(Button)findViewById(R.id.signOutBtn);
         signOutBtn.setOnClickListener(this);
         //fAuth = FirebaseAuth.
+        imgBtnBks = (ImageView) findViewById(R.id.imgBtnBks);
+        imgBtnBks.setOnClickListener(this);
 
     }
 
@@ -27,6 +31,10 @@ public class Wall2Actvty extends AppCompatActivity implements View.OnClickListen
         if (v.getId() == R.id.signOutBtn) {
             //fAuth.
             Intent intent2 = new Intent(this,signInActivity.class);
+            startActivity(intent2);
+        }
+        if(v==imgBtnBks){
+            Intent intent2 = new Intent(this,MainActivity.class);
             startActivity(intent2);
         }
     }
