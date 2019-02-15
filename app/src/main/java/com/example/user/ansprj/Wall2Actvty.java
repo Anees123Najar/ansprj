1b73f9635a6a47d4fa01b22dfe4cd2b2f8813463
Anis123Najar <anis.najjar9@gmail.com>
2019-02-15 08:59:02 +0200

    15/2/19

--- app/src/main/java/com/example/user/ansprj/Wall2Actvty.java
+++ app/src/main/java/com/example/user/ansprj/Wall2Actvty.java
@@ -11,18 +11,42 @@ import android.widget.ImageView;
 
 public class Wall2Actvty extends AppCompatActivity implements View.OnClickListener {
     private ImageView imgBtnBks;
+    private ImageView imgBtnChs;
+    private ImageView imgBtnSpr;
+    private ImageView imgBtnGmg;
+    private ImageView imgBtnFod;
+    private ImageView imgBtnMsc;
+
+
+
     private Button signOutBtn;
     FirebaseAuth fAuth;// = FirebaseAuth.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wall_actvty);
+
         signOutBtn=(Button)findViewById(R.id.signOutBtn);
         signOutBtn.setOnClickListener(this);
         //fAuth = FirebaseAuth.
+
         imgBtnBks = (ImageView) findViewById(R.id.imgBtnBks);
         imgBtnBks.setOnClickListener(this);
 
+        imgBtnChs = (ImageView) findViewById(R.id.imgBtnChs);
+        imgBtnChs.setOnClickListener(this);
+
+        imgBtnSpr = (ImageView) findViewById(R.id.imgBtnSpr);
+        imgBtnSpr.setOnClickListener(this);
+
+        imgBtnGmg = (ImageView) findViewById(R.id.imgBtnGmg);
+        imgBtnGmg.setOnClickListener(this);
+
+        imgBtnFod = (ImageView) findViewById(R.id.imgBtnFod);
+        imgBtnFod.setOnClickListener(this);
+
+        imgBtnMsc = (ImageView) findViewById(R.id.imgBtnMsc);
+        imgBtnMsc.setOnClickListener(this);
     }
 
 
@@ -34,7 +58,25 @@ public class Wall2Actvty extends AppCompatActivity implements View.OnClickListen
             startActivity(intent2);
         }
         if(v==imgBtnBks){
-            Intent intent2 = new Intent(this,MainActivity.class);
+            Intent intent2 = new Intent(this,ReadingActivity.class);
+            startActivity(intent2);
+        }
+        if(v==imgBtnChs){
+            Intent intent2 = new Intent(this,ChessActivity.class);
+            startActivity(intent2);
+        }
+        if(v==imgBtnSpr){
+            Intent intent2 = new Intent(this,SportsActivity.class);
+            startActivity(intent2);
+        }
+        if(v==imgBtnGmg){
+            Intent intent2 = new Intent(this,GamingActivity.class);
+            startActivity(intent2);
+        }if(v==imgBtnFod){
+            Intent intent2 = new Intent(this,FoodieActivity.class);
+            startActivity(intent2);
+        }if(v==imgBtnMsc){
+            Intent intent2 = new Intent(this,MusicActivity.class);
             startActivity(intent2);
         }
     }
