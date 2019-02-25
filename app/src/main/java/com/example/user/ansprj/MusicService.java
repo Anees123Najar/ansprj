171e3881b317b2bc995e414a7df7e3e715767d72
Anis123Najar <anis.najjar9@gmail.com>
2019-02-25 12:58:44 +0200

    25/02/19

---
+++ app/src/main/java/com/example/user/ansprj/MusicService.java
@@ -0,0 +1,36 @@
+package com.example.user.ansprj;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+public class MusicService extends Service {
+    private MediaPlayer mp;
+    private static boolean isRunning=true;
+    public MusicService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) { return null;}
+    public void onCreate(){
+        super.onCreate();
+        //mp=MediaPlayer.create(getApplicationContext(),R.raw.besound_sweet);
+    }
+
+    public int onStartCommand(Intent intent,int flags,int startId) {
+        mp.setLooping(true);
+        mp.start();
+        isRunning=true;
+        return START_NOT_STICKY;
+    }
+    @Override
+    /**
+     * Destroy and stop music
+     */
+    public void onDestroy() {
+     mp.stop();
+     isRunning=false;
+     super.onDestroy();
+    }
+}
